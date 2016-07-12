d4c2e942d84ef8105d56cbc1de970eb714095f62
kkk99512 <kkk99512@hotmail.com>
2016-07-12 16:30:40 +0800

    Delete Functions.h

--- Functions.h
+++
@@ -1,8 +0,0 @@
-#include "Structs.h"
-bool MySort1 (Point lhs, Point rhs);
-bool MySort2 (Segment lhs, Segment rhs);
-
-bool bCross (Point L,Point R);
-
-void FindLinearEquation(Point &p);
-void InitialY(vector <Point> &vY);
